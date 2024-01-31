ec9ec75bace4c7b4144fcbacb76c7b4c060dcfbf
laurentiu667 <dilionlaur144@gmail.com>
2024-01-31 15:51:06 -0500

    jnavaaaa

--- OOP2/projet1/app/src/main/java/com/example/login/Annexe2.java
+++ OOP2/projet1/app/src/main/java/com/example/login/Annexe2.java
@@ -6,13 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
 
+import java.lang.reflect.Array;
+
 public class Annexe2 extends AppCompatActivity {
     Ecouteur ec;
 
-    Button boutonValider;
-    EditText champNomCompte;
-    TextView champSolde;
-
+    Button boutonValiderVar;
+    EditText champNomCompteVar;
+    TextView champSoldeVar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +21,13 @@ public class Annexe2 extends AppCompatActivity {
         setContentView(R.layout.activity_annexe2);
 
         // initialiser les variables
-        boutonValider = findViewById(R.id.validebutton);
-        champNomCompte = findViewById(R.id.champNomCompte);
-        champSolde = findViewById(R.id.champSolde);
+        boutonValiderVar = findViewById(R.id.validebutton);
+        champNomCompteVar = findViewById(R.id.champNomCompte);
+        champSoldeVar = findViewById(R.id.champSolde);
 
         // 1 er etape : creation de l ecouteur
         ec = new Ecouteur();
-        boutonValider.setOnClickListener(ec);
+        boutonValiderVar.setOnClickListener(ec);
 
 
 
@@ -37,8 +38,20 @@ public class Annexe2 extends AppCompatActivity {
 
         @Override
         public void onClick(View v) {
-            champSolde.setText(234324);
+
+            String valeurText = champNomCompteVar.getText().toString();
+            System.out.println(valeurText);
+            if (valeurText.equals("Cheque") || valeurText.equals("Epargne") || valeurText.equals("EpargnePlus")){
+                champSoldeVar.setText("21312");
+            } else {
+                champSoldeVar.setText("faux");
+            }
         }
+
+
+
+
     }
+
 
 }
