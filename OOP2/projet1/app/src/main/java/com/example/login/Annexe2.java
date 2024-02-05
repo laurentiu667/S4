14a2e68bf6e17ea47e5c2704e5393c0f3f690f2a
Laurentiu667 <dilionlaur144@gmail.com>
2024-02-05 12:25:17 -0500

    ooooop

--- OOP2/projet1/app/src/main/java/com/example/login/Annexe2.java
+++ OOP2/projet1/app/src/main/java/com/example/login/Annexe2.java
@@ -6,7 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
 
-import java.lang.reflect.Array;
+import java.util.Locale;
+import java.util.Vector;
 
 public class Annexe2 extends AppCompatActivity {
     Ecouteur ec;
@@ -14,6 +15,12 @@ public class Annexe2 extends AppCompatActivity {
     Button boutonValiderVar;
     EditText champNomCompteVar;
     TextView champSoldeVar;
+
+    TextView mail;
+    TextView Atransfer;
+    Button boutonEnvoyerVar;
+    Vector<String> choix;
+    Double solde_global;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +32,21 @@ public class Annexe2 extends AppCompatActivity {
         champNomCompteVar = findViewById(R.id.champNomCompte);
         champSoldeVar = findViewById(R.id.champSolde);
 
+        choix = new Vector();
+        choix.add("CHEQUE");
+        choix.add("EPARGNE");
+        choix.add("EPARGNEPLUS");
+
+
+        mail = findViewById(R.id.mail);
+        Atransfer = findViewById(R.id.transferA);
+
+        boutonEnvoyerVar = findViewById(R.id.envoyerButton);
+
         // 1 er etape : creation de l ecouteur
         ec = new Ecouteur();
         boutonValiderVar.setOnClickListener(ec);
+        boutonEnvoyerVar.setOnClickListener(ec);
 
 
 
@@ -38,14 +57,28 @@ public class Annexe2 extends AppCompatActivity {
 
         @Override
         public void onClick(View v) {
+            solde_global = 500.0;
 
-            String valeurText = champNomCompteVar.getText().toString();
-            System.out.println(valeurText);
-            if (valeurText.equals("Cheque") || valeurText.equals("Epargne") || valeurText.equals("EpargnePlus")){
-                champSoldeVar.setText("21312");
-            } else {
-                champSoldeVar.setText("faux");
+            if (v == boutonValiderVar){
+                String nom = String.valueOf(champNomCompteVar.getText());
+                nom = nom.trim().toUpperCase();
+                if (choix.contains(nom)){
+                    champSoldeVar.setText(String.valueOf(solde_global));
+                }
+                else {
+                    champSoldeVar.setText("Pas le bon compte");
+                    champNomCompteVar.setText("");
+                }
+            } else if (v == boutonEnvoyerVar){
+                if (mail.length() != 0 && Atransfer.length() != 0){
+                    solde_global -= solde_global;
+                    champSoldeVar.setText(String.valueOf(solde_global));
+                    mail.setText("");
+                    Atransfer.setText("");
+                }
             }
+
+
         }
 
 
