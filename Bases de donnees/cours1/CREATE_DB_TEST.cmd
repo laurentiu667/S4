940af700f84ab8517afd98151626cb7a4b57ed58
laurentiu667 <dilionlaur144@gmail.com>
2024-02-04 21:41:29 -0500

    base de donne

--- Bases de donnees/cours1/CREATE_DB_TEST.cmd
+++ Bases de donnees/cours1/CREATE_DB_TEST.cmd
@@ -1,13 +1,13 @@
 @ECHO OFF
 
 REM Mot de passe de l'utilisateur "postgres"
-SET PGPASSWORD=AAAaaa123
+SET PGPASSWORD=jaune
 
 SET /p "nom_fichier=Entrez le nom du fichier : "
 
 ECHO Vous avez saisi : "%nom_fichier%"
 
 REM Commande qui supprime la BD "DB_TEST" et recréer la BD "DB_TEST"
-"c:\Program Files\PostgreSQL\15\bin\psql.exe" -U postgres < %nom_fichier%
+"C:\Program Files\PostgreSQL\16\bin\psql.exe" -U postgres -f %nom_fichier%
 
 PAUSE
