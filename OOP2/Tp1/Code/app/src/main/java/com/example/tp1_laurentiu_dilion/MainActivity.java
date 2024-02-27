d9f491416cec79d128f4d71d1ba30e570dd166fc
laurentiu667 <dilionlaur12@gmail.com>
2024-02-27 12:44:05 -0500

    waoiwor

--- OOP2/Tp1/Code/app/src/main/java/com/example/tp1_laurentiu_dilion/MainActivity.java
+++ OOP2/Tp1/Code/app/src/main/java/com/example/tp1_laurentiu_dilion/MainActivity.java
@@ -2,33 +2,22 @@ package com.example.tp1_laurentiu_dilion;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.google.android.material.chip.Chip;
-import com.google.android.material.chip.ChipGroup;
-import com.example.tp1_laurentiu_dilion.ListeProduits;
-import com.example.tp1_laurentiu_dilion.Commande;
-
 import java.text.DecimalFormat;
-import java.util.Set;
-
 
 public class MainActivity extends AppCompatActivity {
 
     // Declaration des variables
-    ImageView filtre, americano, glace, latte, enAttente;
+    ImageView filtre, americano, glace, latte;
     Chip petit, moyen, grand;
     Button ajouterCaisse, effacerCaisse, passerCommande;
     TextView detailcommande, totalargent;
@@ -55,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         ImageCaisseCaffe = findViewById(R.id.ImageCaisseCaffe);
         totalargent = findViewById(R.id.totalargent);
 
-        // listeners
+        // écouteur pour chaque composants
         ec = new Ecouteur();
         filtre.setOnClickListener(ec);
         americano.setOnClickListener(ec);
@@ -82,79 +71,77 @@ public class MainActivity extends AppCompatActivity {
                 String cle = nomProduit + " " + tailleProduit;
                 Cafe cafeChoisi = listeProduits.getCafe(cle);
                 String nombreFormate = df.format(cafeChoisi.getPrix());
-                detailcommande.setText("Café " + nomProduit + " " + cafeChoisi.getCalorie() + " cal " + nombreFormate);
+                detailcommande.setText("Café " + nomProduit + " " + cafeChoisi.getCalorie() + " cal " + tailleProduit + " " + nombreFormate);
                 return cafeChoisi;
             }
         }
 
         return null;
     }
+
     private class Ecouteur implements View.OnClickListener {
         String nomProduit = null;
-
         String tailleProduit = "petit";
         Commande commande = new Commande();
         ImageView enAttente = null;
+
         @Override
         public void onClick(View v) {
-
-            // Recuperer le nom de l image pour chaque
-            if (v instanceof ImageView){
+            // Recuperer le nom de l'image pour chaque
+            if (v instanceof ImageView) {
                 nomProduit = v.getTag().toString();
                 Drawable drawable = ((ImageView) v).getDrawable();
                 enAttente = new ImageView(MainActivity.this);
-
                 if (drawable != null) {
                     enAttente.setImageDrawable(drawable);
                 }
             } else if (v instanceof Chip) {
                 ((Chip) v).setChipIconResource(R.drawable.crochet);
-                tailleProduit =v.getTag().toString();
+                tailleProduit = v.getTag().toString();
             }
-
-
+            // AjouterProduit retourne le bon café grâce à la clé de la hashtable et la stocke dans une variable cafe
             Cafe cafe = ajouterProduit(nomProduit, tailleProduit);
+
             if (v.getId() == R.id.ajouterCaisse) {
+                // Ajouter le type de café ( produit ) pour calculer son prix + taxes + calories
                 commande.ajouter_boisson(cafe);
                 totalargent.setText(String.format("%.2f $", commande.totalTaxes()));
+
+                // Reinitialisation de la taille du café
                 petit.setChipIconResource(R.drawable.crochet);
                 tailleProduit = "petit";
 
+                // Ajout du café ( image ) dans la caisse
 
-                LinearLayout layout = new LinearLayout(MainActivity.this);
+                ImageView img = new ImageView(MainActivity.this);
+                img.setImageDrawable(enAttente.getDrawable());
+                img.setLayoutParams(new LinearLayout.LayoutParams(150, 150));
+                img.setPadding(10, 10, 10, 10);
 
-                layout.addView(enAttente);
+                // Ajoutez l'image au LinearLayout parent
+                ImageCaisseCaffe.addView(img);
 
-                layout.setLayoutParams();
 
-                ImageCaisseCaffe.addView(layout);
-
-            }
-            else if (v.getId() == R.id.effacerCaisse) {
+            } else if (v.getId() == R.id.effacerCaisse) {
                 commande.reset();
+
                 totalargent.setText(commande.getTotal() + " $");
                 detailcommande.setText("Détails de la commande");
                 ajouterCaisse.setEnabled(false);
                 ajouterCaisse.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#96B1A8")));
                 petit.setChipIconResource(R.drawable.crochet);
-
+                ImageCaisseCaffe.removeAllViews();
                 tailleProduit = "petit";
             } else if (v.getId() == R.id.passerCommande) {
-
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                 builder.setTitle("Commande envoyée");
                 builder.setMessage("paiement de " + String.format("%.2f $", commande.getTotal()) + " en cours");
                 builder.show();
             }
 
-
-            if (v.getId() != R.id.effacerCaisse){
+            if (v.getId() != R.id.effacerCaisse) {
                 ajouterProduit(nomProduit, tailleProduit);
             }
-
         }
-
-
     }
 }
-
